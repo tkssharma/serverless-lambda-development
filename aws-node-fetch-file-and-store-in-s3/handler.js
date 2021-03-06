39a87c89007c740a0211f9b1df5128d7ded8de03
tkssharma <tarun.softengg@gmail.com>
2021-03-07 00:52:55 +0530

    lambda example added with s3

---
+++ aws-node-fetch-file-and-store-in-s3/handler.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+const AWS = require('aws-sdk'); 
+const s3 = new AWS.S3();
+module.exports.save = (event, context, callback) => {
+  fetch(event.image_url)
+    .then((response) => {
+      if (response.ok) {
+        return response;
+      }
+      return Promise.reject(new Error(
+            `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`));
+    })
+    .then(response => response.buffer())
+    .then(buffer => (
+      s3.putObject({
+        Bucket: process.env.BUCKET,
+        Key: event.key,
+        Body: buffer,
+      }).promise()
+    ))
+    .then(v => callback(null, v), callback)
+    .catch(err =>  callback(err, v), callback)
+};
